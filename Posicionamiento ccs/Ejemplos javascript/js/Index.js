6946085da6aa60244b07a4b78740a4a1af427abf
gustavo-cortesprog <al45335@un.edu.mx>
2024-12-02 11:12:56 -0600

    Cambios de bash

---
+++ Posicionamiento ccs/Ejemplos javascript/js/Index.js
@@ -0,0 +1,7 @@
+// console.log("Hola Mundo desde mi archivo")
+// alert("Esto es un DM para el mundo")
+
+console.log(5+360)
+console.warn("Estamos dando tips para los alumnos de IDAI")
+console.info("Esto es muy importante")
+console.error("No debi haber estudiado psicologia")
